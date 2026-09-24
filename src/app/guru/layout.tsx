b9afae8d663b91d8de8a7ea7e983ca5286e61062
bertalagi94-cmd <bertalagi94@gmail.com>
2026-09-25 07:17:04 +0800

    Update layout.tsx

--- src/app/guru/layout.tsx
+++ src/app/guru/layout.tsx
@@ -4,7 +4,7 @@ import { useEffect } from 'react'
 import { useRouter } from 'next/navigation'
 import { GuruSidebar } from '@/components/shared/Sidebar'
 import { SesiTerlupaPopup } from '@/components/shared/SesiTerlupaPopup'
-import { FullscreenButton } from '@/components/shared/FullscreenButton'
+import { TopBarControls } from '@/components/shared/TopBarControls'
 import { ViewAsBanner } from '@/components/shared/ViewAsBanner'
 import { DashboardPhotoBackground } from '@/components/shared/DashboardPhotoBackground'
 
@@ -76,7 +76,7 @@ export default function GuruLayout({ children }: { children: React.ReactNode }) 
       </svg>
       <div className="relative z-10 flex w-full">
         <GuruSidebar />
-        <FullscreenButton />
+        <TopBarControls accent="#059669" />
         <main className="flex-1 min-w-0 p-6 lg:p-8 pt-16 lg:pt-16">
           {children}
         </main>
